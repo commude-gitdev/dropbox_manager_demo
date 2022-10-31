a6101dc45d74840ddb8daf3bd75ff0fb289eb009
Huy Hoàng <92375121+hoang-t3k56qx1@users.noreply.github.com>
2022-11-01 06:44:21 +0700

    commit v5

--- app/src/main/java/com/hoangt3k56/dropbox/activity/LoginActivity.java
+++ app/src/main/java/com/hoangt3k56/dropbox/activity/LoginActivity.java
@@ -3,17 +3,26 @@ package com.hoangt3k56.dropbox.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.dropbox.core.android.Auth;
 import com.hoangt3k56.dropbox.R;
+import com.hoangt3k56.dropbox.api.ApiService;
+import com.hoangt3k56.dropbox.model.Enty;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
     public static final String KEY="sv2abu0lwbaoed6";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Auth.startOAuth2Authentication(getApplicationContext(),KEY);
+//                Intent intent = new Intent(LoginActivity.this, WebVActivity.class);
+//                startActivity(intent);
+
+//                String token = "Bearer sl.BRx5weF0KD9_qzXKdZzzhttGUDKZFzqNx7XvofWCPtTyz2Yf-PijY2xn21VzLAyp8I_ms6mxo13gG5qMjUdCLcLhiNS2f2mJ2QdEqbsMDg5XcRNPjUb2QxUGLMo2GBOMQCKaq6BUgTbg";
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                intent.putExtra("TOKEN",token);
+//                startActivity(intent);
             }
         });
     }
@@ -32,11 +48,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         String token = Auth.getOAuth2Token();
-
+        Log.d("hoangdev", "tocken  " + token);
         if(token!=null)
         {
             Intent intent=new Intent(this, MainActivity.class);
-            intent.putExtra("TOKEN",token);
+            intent.putExtra("TOKEN","Bearer " + token);
             startActivity(intent);
         }
     }
